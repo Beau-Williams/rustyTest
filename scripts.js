c1083d74f19660d0310bc9c0e48d48dcdb27809d
Beau Williams <115419853+Beau-Williams@users.noreply.github.com>
2022-11-06 15:36:09 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,37 +1,76 @@
-
-import myJSON from './products.json' assert {type: 'json'};
-
-let search = document.getElementById("search");
-let button = document.getElementById("sub");
-let result = document.getElementById("result");
-let del = document.getElementById("del");
-
-
-button.addEventListener("click", function(){
-    let searchItem = search.value;
-    
-    for(let i in myJSON){
-        if(myJSON[i].name.includes(searchItem)){
-            let list = document.createElement("p");
-            let text = document.createTextNode("Item: " + myJSON[i].name + " | Price: " + myJSON[i].price + " | Qty in stock: " + myJSON[i].qty + " | Department: " + myJSON[i].dept); 
-            list.appendChild(text);
-            result.appendChild(list);
-
-            // document.querySelector("#result").innerHTML ="<p>" + myJSON[i].name + " Price: " + myJSON[i].price + " Qty: " + myJSON[i].qty + "</p>";
+$(function(){
+    let shoppingList = [];
+    let products = [
+        {
+            "name" : "apples",
+            "dept" : "produce",
+            "price" : 1.25,
+            "qty" : 25,
+            "img" : "/imgs/dig.jpg"
+        },
+        {
+            "name" : "bananas",
+            "dept" : "produce",
+            "price" : 0.69,
+            "qty" : 100,
+            "img" : "/imgs/dig.jpg"
+        },
+        {
+            "name" : "oranges",
+            "dept" : "produce",
+            "price" : 1.50,
+            "qty" : 125,
+            "img" : "/imgs/dig.jpg"
+        },
+        {
+            "name" : "hiland whole milk",
+            "dept" : "dairy",
+            "price" : 4.99,
+            "qty" : 25,
+            "img" : "/imgs/dig.jpg"
+        },
+        {
+            "name" : "hiland 2% milk",
+            "dept" : "dairy",
+            "price" : 4.25,
+            "qty" : 30,
+            "img" : "/imgs/dig.jpg"
+        },
+        {
+            "name" : "digiorno frozen pizza",
+            "dept" : "frozen",
+            "price" : 5.25,
+            "qty" : 20,
+            "img" : "/imgs/dig.jpg"
         }
         
-    }
+    ];
+    
+    $("#sub").on("click", function(){
+        let search = $("#search").val();
+        
+        for(let i in products){
+            if(products[i].name.includes(search)){
+                $("#resultList").append(
+                    
+                    "<li id='items'>"+"<img src='" + products[i].img + "'>" +products[i].name +
+                        "<br>&emsp;Department: " + products[i].dept +
+                        "<br>&emsp;Price: " + products[i].price +
+                        "<br>&emsp;Qty in Stock" + products[i].qty +
+                    "</li>"   
+                    );
+            }
+        }
+        
+    })
+    $("#del").on("click", function(){
+        $("#resultList").empty();
+    })
+
+  
+
+
+
+
+
 })
-function delChildren() {
-    let e = result;
-    var child = e.lastElementChild;
-    while(child){
-        e.removeChild(child);
-        child = e.lastElementChild;
-    }
-}
-
-del.addEventListener("click", function(){
-    delChildren();
-});
-
