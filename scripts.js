0290fdfb97f4a982fca2192eb9b42d47c7dad7d4
Beau Williams <115419853+Beau-Williams@users.noreply.github.com>
2022-11-10 21:11:44 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 $(function(){
     let shoppingList = [];
+    let resultList =[];
     let products = [
         {
             "name" : "Apples",
@@ -185,24 +186,52 @@ $(function(){
         }
         
     ];
+    
+    let quotes = [
+        {
+            "quote": "I love spending my time at Rusty's!",
+            "name": "Diana King"
+        },
+        {
+            "quote": "I can get in and out, no problem!",
+            "name": "John Smith"
+        }
+    ]
     //Product Search Functionality
     $("#sub").on("click", function(){
-        $("#resultList").empty();
+        $("#result").empty();
         let search = $("#search").val();
-        
+        //had to add a variable for the addList ID to allow the click events to register and record value properly
+        //to format in css #prodInfo button {}
+        let j =0;
         for(let i in products){
+            
             if(products[i].name.toLowerCase().includes(search.toLowerCase())){
-                $("#resultList").append(
-                    
-                    "<li id='items'>"+"<img src='" + products[i].img + "'>" +products[i].name +
-                        "<br>&emsp;Department: " + products[i].dept +
-                        "<br>&emsp;Price: " + products[i].price +
-                        "<br>&emsp;Qty in Stock: " + products[i].qty +
-                    "</li>"   
+                
+                $("#result").append(
+                    "<div id='prodDisp'>" +
+                    "<img src='" + products[i].img + "' id='prodImg'>" + 
+                        '<div id=prodInfo">' +
+                            "<p id='prodName'>&emsp;" + products[i].name + "</p>" +
+                            "<p id='prodDept'>&emsp;Dept: " +products[i].dept + "</p>" +
+                            "<p id='prodPric'>&emsp;Price: " + products[i].price + "</p>" +
+                            "<p id='prodQty'>&emsp;Qty in Stock: " + products[i].qty + "</p>" +
+                            "<button id='addList"+j+"' value='' >Add to Shopping List</button>" +
+                        "</div>" +
+                    "</div>"
                     );
+                    //adds button that pushes that item to shopping list
+                $('#addList' + j).on('click', event => {
+                    let n = products[i].name;
+                    shoppingList.push(n);
+                    
+                    
+                })
+                j++;
+                    
             }
         }
-        if($("#items").length == 0){
+        if($("#result").length == 0){
             $("#resultList").append(
                 "<p style='color:red'>No matching items</p>"
             )
@@ -210,81 +239,113 @@ $(function(){
         
         
     })
-    
+  
 
   //Department Search Functionality
   $("#meat").on("click", function(){
-    $("#depResultList").empty();
+    $("#departmentResult").empty();
     for(let i in products){
         if(products[i].dept.includes("Meat")){
-            $("#depResultList").append(
-                "<li id='items'>"+"<img src='" + products[i].img + "'>" +products[i].name +
-                        "<br>&emsp;Price: " + products[i].price +
-                        "<br>&emsp;Qty in Stock: " + products[i].qty +
-                    "</li>" 
+            $("#departmentResult").append(
+                "<div id='prodDisp'>" +
+                    "<img src='" + products[i].img + "' id='prodImg'>" + 
+                        '<div id="prodInfo">' +
+                            "<p id='prodName'>&emsp;" + products[i].name + "</p>" +
+                            "<p id='prodDept'>&emsp;Dept: " +products[i].dept + "</p>" +
+                            "<p id='prodPric'>&emsp;Price: " + products[i].price + "</p>" +
+                            "<p id='prodQty'>&emsp;Qty in Stock: " + products[i].qty + "</p>" +
+                        "</div>" +
+                "</div>"
             )
             
         }
     }
   })
   $("#home").on("click", function(){
-    $("#depResultList").empty();
+    $("#departmentResult").empty();
     for(let i in products){
         if(products[i].dept.includes("Home")){
-            $("#depResultList").append(
-                "<li id='items'>"+"<img src='" + products[i].img + "'>" +products[i].name +
-                        "<br>&emsp;Price: " + products[i].price +
-                        "<br>&emsp;Qty in Stock: " + products[i].qty +
-                    "</li>" 
+            $("#departmentResult").append(
+                "<div id='prodDisp'>" +
+                    "<img src='" + products[i].img + "' id='prodImg'>" + 
+                        '<div id="prodInfo">' +
+                            "<p id='prodName'>&emsp;" + products[i].name + "</p>" +
+                            "<p id='prodDept'>&emsp;Dept: " +products[i].dept + "</p>" +
+                            "<p id='prodPric'>&emsp;Price: " + products[i].price + "</p>" +
+                            "<p id='prodQty'>&emsp;Qty in Stock: " + products[i].qty + "</p>" +
+                        "</div>" +
+                "</div>"
             )
             
         }
     }
   })
   $("#dairy").on("click", function(){
-    $("#depResultList").empty();
+    $("#departmentResult").empty();
     for(let i in products){
         if(products[i].dept.includes("Dairy")){
-            $("#depResultList").append(
-                "<li id='items'>"+"<img src='" + products[i].img + "'>" +products[i].name +
-                        "<br>&emsp;Price: " + products[i].price +
-                        "<br>&emsp;Qty in Stock: " + products[i].qty +
-                    "</li>" 
+            $("#departmentResult").append(
+                "<div id='prodDisp'>" +
+                    "<img src='" + products[i].img + "' id='prodImg'>" + 
+                        '<div id="prodInfo">' +
+                            "<p id='prodName'>&emsp;" + products[i].name + "</p>" +
+                            "<p id='prodDept'>&emsp;Dept: " +products[i].dept + "</p>" +
+                            "<p id='prodPric'>&emsp;Price: " + products[i].price + "</p>" +
+                            "<p id='prodQty'>&emsp;Qty in Stock: " + products[i].qty + "</p>" +
+                        "</div>" +
+                "</div>"
             )
             
         }
     }
   })
   $("#frozen").on("click", function(){
-    $("#depResultList").empty();
+    $("#departmentResult").empty();
     for(let i in products){
         if(products[i].dept.includes("Frozen")){
-            $("#depResultList").append(
-                "<li id='items'>"+"<img src='" + products[i].img + "'>" +products[i].name +
-                        "<br>&emsp;Price: " + products[i].price +
-                        "<br>&emsp;Qty in Stock: " + products[i].qty +
-                    "</li>" 
+            $("#departmentResult").append(
+                "<div id='prodDisp'>" +
+                    "<img src='" + products[i].img + "' id='prodImg'>" + 
+                        '<div id="prodInfo">' +
+                            "<p id='prodName'>&emsp;" + products[i].name + "</p>" +
+                            "<p id='prodDept'>&emsp;Dept: " +products[i].dept + "</p>" +
+                            "<p id='prodPric'>&emsp;Price: " + products[i].price + "</p>" +
+                            "<p id='prodQty'>&emsp;Qty in Stock: " + products[i].qty + "</p>" +
+                        "</div>" +
+                "</div>"
             )
             
         }
     }
   })
   $("#produce").on("click", function(){
-    $("#depResultList").empty();
+    $("#departmentResult").empty();
     for(let i in products){
         if(products[i].dept.includes("Produce")){
-            $("#depResultList").append(
-                "<li id='items'>"+"<img src='" + products[i].img + "'>" +products[i].name +
-                        "<br>&emsp;Price: " + products[i].price +
-                        "<br>&emsp;Qty in Stock: " + products[i].qty +
-                    "</li>" 
+            $("#departmentResult").append(
+                "<div id='prodDisp'>" +
+                    "<img src='" + products[i].img + "' id='prodImg'>" + 
+                        '<div id="prodInfo">' +
+                            "<p id='prodName'>&emsp;" + products[i].name + "</p>" +
+                            "<p id='prodDept'>&emsp;Dept: " +products[i].dept + "</p>" +
+                            "<p id='prodPric'>&emsp;Price: " + products[i].price + "</p>" +
+                            "<p id='prodQty'>&emsp;Qty in Stock: " + products[i].qty + "</p>" +
+                        "</div>" +
+                "</div>"
             )
             
         }
     }
   })
-
-
+//show shopping list array
+  $("#showList").on("click",function(){
+    window.localStorage.setItem('item', JSON.stringify(shoppingList))
+    for(let i of shoppingList){
+        console.log(i);
+        
+    }
+    console.log(JSON.parse(window.localStorage.getItem('item')))
+  })
 
 
 
